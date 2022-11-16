36457baaae8a69d88d480bb0f922670df8555c7d
«Vadim <vvaganoff@mail.ru>
2022-11-16 23:11:27 +0400

    добавлено решение задачи номер 13

---
+++ Задача 13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write( "Введите число: ");
+int number = Convert.ToInt32( Console.ReadLine());
+string strNumber = Convert.ToString( number );
+
+if (number > 99) 
+{
+    Console.WriteLine( $"Третья цифра:  {strNumber[2]}" );
+}
+else
+{
+    Console.WriteLine( "В этом числе нет третьей цифры!" );
+}
