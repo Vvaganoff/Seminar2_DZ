a6c47a28675a24ef3291904d3d8376e8f7ed4856
«Vadim <vvaganoff@mail.ru>
2022-11-16 22:58:04 +0400

    добавлено решение задачи номер 10

---
+++ Задача 10/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write( "Введите трёхзначное число: ");
+int number = Convert.ToInt32( Console.ReadLine());
+if (number > 99 &&  number < 1000) 
+{
+    Console.WriteLine( $"Вторая цифра:  {number % 100 / 10}" );
+}
+else
+{
+    Console.WriteLine( "Это число не трёхзначное!" );
+}
