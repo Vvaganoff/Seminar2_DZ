3e622f708c5a43fdc7a74be2832b6163b5d4c602
«Vadim <vvaganoff@mail.ru>
2022-11-16 23:28:19 +0400

    добавлено решение задачи номер 15

---
+++ Задача 15/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите номер дня недели: ");
+int dayNum = Convert.ToInt32(Console.ReadLine());
+if (dayNum > 0 && dayNum < 8)
+{
+    if (dayNum > 5) 
+    {
+        Console.WriteLine("Да!");
+    }
+    else
+    {
+        Console.WriteLine("Нет!");
+    }
+}
+else
+{
+    Console.WriteLine("Нет такого дня недели.");
+}
